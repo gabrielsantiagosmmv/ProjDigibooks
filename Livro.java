026c9f9f4eab3945cc34d8c2371879971eece7fc
Gabriel Santiago <gabsantestudos@gmail.com>
2025-11-20 12:59:31 -0300

    asdasd

--- Livro.java
+++
@@ -1,9 +0,0 @@
-package digibooks.models;
-
-public class Livro {
-    private int idLivro;
-    private String titulo;
-    private String autor;
-    private int idCategoria;
-    private int anoPublicacao;
-    private String status;
