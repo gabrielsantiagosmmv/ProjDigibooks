5739be4368f79579ad0b87e636f2687aff3e8ea4
Diogo <estudosadsdiogoav@gmail.com>
2025-11-20 12:53:30 -0300

    categoria e mov

---
+++ Categoria.java
@@ -0,0 +1,15 @@
+// Implementar Categoria.java
+package digibooks.models;
+
+public class Categoria {
+    private int idCategoria;
+    private String descricao;
+    
+    // Construtor, Getters e Setters
+}
+
+// Implementar Movimentacao.java
+package digibooks.models;
+
+import java.time.LocalDate;
+
