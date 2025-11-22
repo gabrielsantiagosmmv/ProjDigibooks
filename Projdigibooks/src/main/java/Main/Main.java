d9a08702f5b2b588fc5303967f8613d1d3aef07a
Diogo <estudosadsdiogoav@gmail.com>
2025-11-22 14:24:56 -0300

    main

--- Projdigibooks/src/main/java/Main/Main.java
+++ Projdigibooks/src/main/java/Main/Main.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Main;
+import javax.swing.SwingUtilities;
 
-/**
- *
- * @author Gabriel
- */
 public class Main {
-    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new BibliotecaGUI().setVisible(true);
+        });
+    }
 }
